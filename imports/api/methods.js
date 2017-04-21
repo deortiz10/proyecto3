804f8fe1d4cfe2a856c316b4d1236e5f591b6439
Luis Mesa <la.mesa10@uniandes.edu.co>
2017-04-20 23:02:42 -0500

    Code review

--- imports/api/methods.js
+++ imports/api/methods.js
@@ -9,6 +9,7 @@ Meteor.methods({
 		console.log("Search walmart");
 		check(query, String);
 		//let myWalmart = walmart(process.env.WALMART_API_KEY);
+		//El API_Key no debería estar quemado en codigo
 		let myWalmart = walmart('acc828nd6mdxeqwqt7fz5s7m');
     // Return the promise
     return myWalmart.search(query);
@@ -17,7 +18,9 @@ Meteor.methods({
     "recommendations"(query) {
         console.log("Recomend walmart");
 				console.log(query);
+        //La concatenación no debería estar dentro del check
         check(""+query, String);
+      //El API_Key no debería estar quemado en codigo
 				let myWalmart = walmart('acc828nd6mdxeqwqt7fz5s7m');
         // Return the promise
         return myWalmart.recommendations(""+query);
@@ -27,6 +30,7 @@ Meteor.methods({
 				console.log("Reviews walmart");
 				console.log(query);
 				check(""+query, String);
+      //El API_Key no debería estar quemado en codigo
 				let myWalmart = walmart('acc828nd6mdxeqwqt7fz5s7m');
 				// Return the promise
 				return myWalmart.reviews(""+query);
